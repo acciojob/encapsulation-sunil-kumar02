eb2054a5987d35e18b9bb2c020956ed7659aab6c
sunil kumar <105sunil2000@gmail.com>
2023-04-18 17:06:12 +0530

    encapsulation done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+
+        //obj.name("sunil")
+
+        obj.setName("sunil");
+        String ans = obj.getName();
+
+        System.out.println(ans);
+    }
+
+
   
 }
